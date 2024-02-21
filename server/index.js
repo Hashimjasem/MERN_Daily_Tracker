e90d1060443565b0a7b0f36eb28d65e9729759ab
Hashimjasem <j_hashim1305@outlook.com>
2024-02-21 14:12:09 +0800

    made changes to server file to connect to db

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const connectDB = require('./dbConnection/db');
 
 const app = express();
+
+// Connect Database
+connectDB();
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
 const port = process.env.PORT || 8082;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server running on ${"http://localhost:8082"}`));
