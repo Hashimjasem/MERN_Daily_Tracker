e97e4f39d180945c40626bb88051029a7e98fd7d
Hashimjasem <j_hashim1305@outlook.com>
2024-03-01 10:40:36 +0800

    User Schema

---
+++ server/models/user.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    first_name: {
+        type: String,
+        required: true
+    },
+    sir_name: {
+        type: String,
+        required: true
+    },
+    dob: {
+        type: Date,
+        required: true
+    },
+    email: {
+        type: String
+    },
+    password: {
+        type: true
+    }
+}, { timestamps: true });
+
+// Hash the password before saving it to the database
+userSchema.pre('save', async function (next) {
+    const user = this;
+    if (!user.isModified('password')) return next();
+
+    try {
+        const salt = await bcrypt.genSalt();
+        user.password = await bcrypt.hash(user.password, salt);
+        next();
+    } catch (error) {
+        return next(error);
+    }
+});
+
+// Compare the given password with the hashed password in the database
+userSchema.methods.comparePassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+module.exports = User = mongoose.model('user', UserSchema);
